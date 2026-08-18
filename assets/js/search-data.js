d1ac9e0e5d170f8a2f51320433c15233c347b23d
gyeomh <johnhwangbo17@gmail.com>
2026-08-18 04:22:17 +0000

    Deploying to gh-pages from @ gyeomh/gyeomh.github.io@f3ef6a0f130a67eb50a14797c3042044138279eb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,7 +64,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%67%79%65%6F%6D%68%77%61%6E%67%62%6F%31@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+          window.open("mailto:%67%79%65%6F%6D%68%77%61%6E%67%62%6F@%70%6F%73%74%65%63%68.%61%63.%6B%72", "_blank");
         },
       },{
       id: 'light-theme',
